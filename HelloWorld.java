cfc5a80da5b3986f206b6c7c71010b27de231f24
blkfinch <blkfinch@gmail.com>
2018-01-16 13:32:09 -0700

    hello!

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+
+	public void sayHello(){
+		System.out.println("hello world!");
+	}
+
+	public static void main(String[] args){
+		HelloWorld hello = new HelloWorld();
+		hello.sayHello();
+
+	}//end of main
+
+}//end of class
