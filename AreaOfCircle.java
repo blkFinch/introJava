906303e85777b1e94548efaa93d5179a4e23a5fe
N. Hutchison <blkfinch@gmail.com>
2018-01-25 12:38:13 -0700

    finished area of circle with OOP

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -10,26 +10,51 @@ public class AreaOfCircle{
 	private double area;
 
 
-	public void getRadius(){
+	public double getRadius(){
 		System.out.print("Enter the radius: ");
 		radius = input.nextDouble();
+		return radius;
 	}
 
-	public void calculateArea(){
+	public void setRadius(double radius){
+		this.radius = radius;
+
+	}
+
+	public double getArea(){
 		area = Math.pow(radius,2) * PI;
+		return area;
+	}
+
+	public void setArea(double area){
+		this.area = area;
 	}
 
 	public void printResult(){
 		System.out.println("The area is " + area);
 	}
 
+	public AreaOfCircle(double radius, double area){
+		super();
+		this.radius = radius;
+		this.area = area;
+	}
+
+	@Override
+	public String toString(){
+		return "The area is " + area; 
+	}
+
+	public void loop(){
+		getRadius();
+		getArea();
+		System.out.println(toString());
+	}
 
 	public static void main(String[] args){
 
-		AreaOfCircle circle = new AreaOfCircle();
-		circle.getRadius();
-		circle.calculateArea();
-		circle.printResult();
+		AreaOfCircle circle = new AreaOfCircle(0,0);
+		circle.loop();
 
 	}//end of main
 }//end of class
